600a57e4d35dfce8727f6570a1ca1456df8a1f7f
joarmanaj <johnuhuarmani0@gmail.com>
2025-01-20 03:46:11 +0100

    add . files

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 let name = "john";
-console.log(name)
+let surname = "Nuhu";
+
+
+console.log(name);
+console.log(surname);
+
+let age = "100";
+console.log(age);
+
+age = "354";
+console.log(age);
+
+console.log(3 + 2) - 76 * (1 + 1);
